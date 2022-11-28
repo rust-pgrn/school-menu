41cf5abc49c90f30b5c9058db1fcd34fd0a5f7d6
rust-pgrn <rust_pgrn@proton.me>
2022-11-28 12:43:28 +0200

    Basic server

--- school-menu-frontend/src/App.tsx
+++ school-menu-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { User } from "./User";
 
 const App: Component = () => {
   const [signedIn, setSignedIn] = createSignal(false);
+  const [isUser, setIsUser] = createSignal();
   const [user, setUser] = createSignal();
   return (
     <>
@@ -11,30 +12,36 @@ const App: Component = () => {
         when={!signedIn()}
         fallback={
           <>
-            <Show when={user()} fallback={<Admin />}>
+            <Show when={isUser()} fallback={<Admin />}>
               <User />
             </Show>
           </>
         }
       >
-        <div>
-          <button
-            onClick={() => {
-              setSignedIn(true);
-              setUser(false);
-            }}
-          >
-            Admin
-          </button>
-          <button
-            onClick={() => {
-              setSignedIn(true);
-              setUser(true);
-            }}
-          >
-            User
-          </button>
-        </div>
+        <form
+          onInput={(event) => {
+            event.preventDefault();
+            setSignedIn(true);
+            console.log(event.target.nodeValue);
+          }}
+        >
+          <label for="students">Who Are You?</label>
+          <select id="students" name="students" size="4">
+            <option value="Mohamed Ali">Mohamed Ali</option>
+            <option value="Karim Tamer">Karim Tamer</option>
+            <option value="Amina Kenzi">Amina Kenzi</option>
+            <option value="Marcus Higenyi">Marcus Higenyi</option>
+            <option value="Sherif Khalifa">Sherif Khalifa</option>
+            <option
+              value="Ms. Aline"
+              onClick={() => {
+                setIsUser(true);
+              }}
+            >
+              Ms. Aline
+            </option>
+          </select>
+        </form>
       </Show>
     </>
   );
--- school-menu-frontend/src/User.tsx
+++ school-menu-frontend/src/User.tsx
@@ -1,34 +1,5 @@
 import { createSignal, Show } from "solid-js";
 
 export function User() {
-  const [choseId, setChoseId] = createSignal(false);
-  return (
-    <>
-      <Show
-        when={choseId()}
-        fallback={
-          <>
-            <form
-              onSubmit={(event) => {
-                event.preventDefault();
-                setChoseId(true);
-              }}
-            >
-              <label for="students">Who Are You?</label>
-              <input list="students" id="browser" />
-              <datalist id="students">
-                <option value="Mohamed Ali" />
-                <option value="Karim Tamer" />
-                <option value="Amina Kenzi" />
-                <option value="Marcus Higenyi" />
-                <option value="Sherif Khalifa" />
-              </datalist>
-            </form>
-          </>
-        }
-      >
-        Menu For Students
-      </Show>
-    </>
-  );
+  return <>Menu For Students</>;
 }
